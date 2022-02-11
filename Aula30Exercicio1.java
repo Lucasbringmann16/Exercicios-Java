22ff27bda2371385575efc486549935040ea2e7d
Lucasbringmann16 <97703708+Lucasbringmann16@users.noreply.github.com>
2022-02-11 14:47:57 -0300

    Update and rename Exercicio1Aula30.java to Aula30Exercicio1.java
    Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
    mensagem explicativa, conforme exemplos.
    
    ENTRADA: 10 e 30          SAIDA: SOMA = 40
    ENTRADA: -30 e 10         SAIDA: SOMA = -20
    ENTRADA: 0 e 0               SAIDA: SOMA = 0

---
+++ Aula30Exercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+    	Scanner sc = new Scanner(System.in);
+		
+	int x;
+	int y;
+	int z;
+		
+	x = sc.nextInt();
+	y = sc.nextInt();
+	z = (x + y);
+		
+	System.out.print("SOMA = ");
+	System.out.println(z);
+		
+  }
+	
+}
